e9d1bb326d6b8b36c92967492c2546a02d06b3a5
Ron Ronaldo Ronalds <65684769+ronalds1979@users.noreply.github.com>
2023-02-05 13:54:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 basic.showIcon(IconNames.Happy)
 TPBot.headlightColor(0xff00ff)
-music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
 basic.forever(function () {
-    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
-    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 15)) {
+    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 60)
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 30)) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 20)
+    } else if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 15)) {
         TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 1)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+        TPBot.headlightRGB(randint(0, 255), randint(0, 255), randint(0, 255))
         TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 0.5)
     }
 })
